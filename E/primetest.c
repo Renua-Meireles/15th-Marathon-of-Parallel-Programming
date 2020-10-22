cfdcf104e115771269212d1036bfe54f7fddbefb
Renua-Meireles <56804974+Renua-Meireles@users.noreply.github.com>
2020-10-22 17:06:10 -0300

    Finished

--- E/primetest.c
+++ E/primetest.c
@@ -91,7 +91,6 @@ int main(int argc,char** argv)
 			primeToTest=atol(strToTest);
 			if (isprime(primeToTest)) 
 			   {
-           //#pragma omp critical
            result[numResults++]=primeToTest;
 			   }
 	      }
